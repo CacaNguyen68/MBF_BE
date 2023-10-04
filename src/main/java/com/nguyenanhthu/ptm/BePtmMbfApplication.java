3f154e6e6581ce5e8ccab7cc90f05ad537d768d3
@cacanguyen68 <thubackend2022@gmail.com>
2023-10-04 09:00:11 +0700

    first

---
+++ src/main/java/com/nguyenanhthu/ptm/BePtmMbfApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenanhthu.ptm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BePtmMbfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BePtmMbfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyenanhthu/ptm/BePtmMbfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenanhthu.ptm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BePtmMbfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
